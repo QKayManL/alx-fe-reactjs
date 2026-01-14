1b81f09c86e5961df030673e8e839b616516c8bd
Emmanuel Dadzie <manueldadzie442@gmail.com>
2026-01-14 19:30:19 +0000

    Task 2: Create Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,7 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 import WelcomeMessage from './components/WelcomeMessage';
 
 import { useState } from 'react'
@@ -10,6 +14,10 @@ function App() {
 
   return (
     <>
+    <Header />
+      <MainContent />
+      <Footer />
+       
     <WelcomeMessage />
 
       <div>
