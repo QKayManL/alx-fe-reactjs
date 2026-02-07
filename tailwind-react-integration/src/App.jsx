dfc72c80e72e2f5b9766806a42242aeca617beeb
Emmanuel Dadzie <manueldadzie442@gmail.com>
2026-02-07 17:11:13 +0000

    Task 1: style UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,7 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind is working
-    </h1>
-  )
+  return <UserProfile />;
 }
 
-export default App
+export default App;
