0da2216ef8b6ce29e2653474b93139f14c8e93d3
Emmanuel Dadzie <manueldadzie442@gmail.com>
2026-02-21 18:05:00 +0000

    Fix: move nested routes into Profile component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -17,8 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route
-          path="/profile"
+        <Route path="/profile/*" 
           element={
             <ProtectedRoute>
               <Profile />
